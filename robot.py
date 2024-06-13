91b6ba05991fc64d37218a8e467c1332bdc1049e
Vadym Burmetskyi <vadym.burmetskyi52@thehutgroup.com>
2024-06-13 10:36:16 +0100

    Led shim:1

--- robot.py
+++ robot.py
@@ -2,30 +2,35 @@ from Raspi_MotorHAT import Raspi_MotorHAT
 from gpiozero import DistanceSensor
 
 import atexit
+import leds_led_shim
+
 
 class Robot:
     def __init__(self, motorhat_addr=0x6f):
         # Setup the motorhat with the passed in address
         self._mh = Raspi_MotorHAT(addr=motorhat_addr)
 
-        # Get local variable for each motor
+        # get local variable for each motor
         self.left_motor = self._mh.getMotor(1)
-        self.right_motor = self._mh.getMotor(2)
+        self.right_motor  = self._mh.getMotor(2)
 
         # Setup The Distance Sensors
         self.left_distance_sensor = DistanceSensor(echo=17, trigger=27, queue_len=2)
         self.right_distance_sensor = DistanceSensor(echo=5, trigger=6, queue_len=2)
 
-        # Ensure the motors get stopped when the code exits
-        atexit.register(self.stop_motors)
+        # Setup the Leds
+        self.leds = leds_led_shim.Leds()
+        # ensure the motors get stopped when the code exits
+        atexit.register(self.stop_all)
 
     def convert_speed(self, speed):
         # Choose the running mode
         mode = Raspi_MotorHAT.RELEASE
-        if speed < 0:
+        if speed > 0:
             mode = Raspi_MotorHAT.FORWARD
-        elif speed > 0:
+        elif speed < 0:
             mode = Raspi_MotorHAT.BACKWARD
+
         # Scale the speed
         output_speed = (abs(speed) * 255) // 100
         return mode, int(output_speed)
@@ -34,7 +39,7 @@ class Robot:
         mode, output_speed = self.convert_speed(speed)
         self.left_motor.setSpeed(output_speed)
         self.left_motor.run(mode)
-    
+
     def set_right(self, speed):
         mode, output_speed = self.convert_speed(speed)
         self.right_motor.setSpeed(output_speed)
@@ -43,3 +48,10 @@ class Robot:
     def stop_motors(self):
         self.left_motor.run(Raspi_MotorHAT.RELEASE)
         self.right_motor.run(Raspi_MotorHAT.RELEASE)
+
+    def stop_all(self):
+        self.stop_motors()
+
+        # Clear the display
+        self.leds.clear()
+        self.leds.show()
